82aa841db4c9f476c09d741f8bb7979cd7af87b1
gaelBZH (PK) <gaelkerninon3110@gmail.com>
2026-03-13 10:17:57 +0100

    Basics

---
+++ basics.py
@@ -0,0 +1,23 @@
+# Variables
+a = 1
+b = 3.14
+c = True
+d = "Hello"
+e = 1j+2
+print(a,b,c,d,e)
+
+# Types
+print(type(a), type(b), type(c), type(d), type(e))
+
+# Operations
+print(a + 2*b)
+print(b - a)
+print(b/b)
+print(b//b)
+print(e*e)
+
+# Built-In Functions
+print(abs(-2.5))
+print(min(1,2))
+print(max(1,2))
+
