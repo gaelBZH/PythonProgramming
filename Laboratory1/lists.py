08b22b241eb65446d5dcb680c038ad21176b964b
gaelBZH (PK) <gaelkerninon3110@gmail.com>
2026-03-13 12:27:26 +0100

    Lists

---
+++ Laboratory1/lists.py
@@ -0,0 +1,18 @@
+# Empty List
+L0 = []
+
+# Examples of Lists
+L1 = [1, 2, 3]
+L2 = [1.0, 3.14]
+L3 = ["Hello", "World", "Hello", "There"]
+
+# Printing Elements
+print(L1)
+print(L1[1])
+
+# Append and Pop
+L = [0, 1, 2, 3]
+L.append(4)
+L.pop()
+L.insert(2, "-1")
+L.pop(2)
