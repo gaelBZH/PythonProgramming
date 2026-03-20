63d52dee584ea5b7c013bb624106eeaf5dd87b95
gaelBZH (PK) <gaelkerninon3110@gmail.com>
2026-03-20 10:42:46 +0100

    Recursion

---
+++ Laboratory2/recursion.py
@@ -0,0 +1,11 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n* factorial(n-1)
+    
+def fibonacci(n):
+    if n == 0 or n == 1:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
